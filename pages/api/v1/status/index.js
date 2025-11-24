452c625edd9b33d385837564db47614cff7bdc5e
Rafael Bernatat Lang <56984193+rafaelbernatat@users.noreply.github.com>
2025-11-24 20:42:00 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,38 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Está funcionando!" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  // "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+
+  const databaseOpendedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_conections: databaseOpendedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,4 @@ function Home() {
   return <h1>A vida é variável, tenha paciência 🤗</h1>;
 }
 
-function About() {
-  console.log("About Page");
-}
-
 export default Home;
